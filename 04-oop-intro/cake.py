5f3e03b057a3c4e6d9c7f4acaef806ceb310aede
slipkova <46921067+slipkova@users.noreply.github.com>
2020-11-30 18:15:40 +0100

    Inheritance and setter /getter

--- 04-oop-intro/cake.py
+++ 04-oop-intro/cake.py
@@ -5,28 +5,28 @@ class Cake:
     default_layers = 3
 
     def __init__(self, l_fl, f_fl):
-        self.layers = Cake.default_layers
+        self.__layers = Cake.default_layers
         self.color = Cake.default_color
         self.layers_flavor = l_fl
         self.filling_flavor = f_fl
         self.decorations = []
 
     def __str__(self):
-        return f'{self.color} cake with {self.layers} {self.layers_flavor} flavored layers and {self.filling_flavor} filling.'.capitalize()
+        return f'{self.color} cake with {self.__layers} {self.layers_flavor} flavored layers and {self.filling_flavor} filling.'.capitalize()
 
     def __eq__(self, other):
         return self.layers_flavor == other.layers_flavor and self.filling_flavor == other.filling_flavor
 
     def __gt__(self, other):
-        return self.layers > other.layers
+        return self.__layers > other.__layers
 
     def __sub__(self, other):
-        l =  self.layers - other.layers
+        l =  self.__layers - other.__layers
         return max(l, -l)
-        #return self.layers - other.layers if self.__gt__(other) else other.layers - self.layers
+        #return self.__layers - other.__layers if self.__gt__(other) else other.__layers - self.__layers
 
     def __add__(self, other):
-        return self.layers + other.layers
+        return self.__layers + other.__layers
 
     def add_decoration(self, name):
         self.decorations.append(name)
@@ -41,9 +41,27 @@ class Cake:
     @classmethod
     def ran_cake(cls):
         x = Cake(Cake.random_flavor(), Cake.random_flavor())
-        x.layers = randrange(2, 10)
+        x.__layers = randrange(2, 10)
         x.color = choice(["white", "blue", "red", "orange"])
         return x
+
+    @property
+    def layers(self):
+        return self.__layers
+
+    @layers.setter
+    def layers(self, value):
+        self.__layers = value
+
+
+class BirthdayCake(Cake):
+    def __init__(self, l_fl, f_fl, can):
+        super().__init__(l_fl, f_fl)
+        self.candles = can
+
+    def __str__(self):
+        return f'{self.color} cake with {self.layers} {self.layers_flavor} flavored layers, {self.filling_flavor} filling and {self.candles} candles.'.capitalize()
+
 
 cake1 = Cake("chocolate", "strawberry")
 print(cake1)
@@ -70,3 +88,6 @@ print(cake2)
 
 cake3 = Cake.ran_cake()
 print(cake3)
+
+cake25 = BirthdayCake("ad", "sdjkhfg", 2)
+print(cake25)
